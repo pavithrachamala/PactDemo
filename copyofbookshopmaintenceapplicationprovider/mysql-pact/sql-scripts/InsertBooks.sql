6fe1fede2c379955302ae42a1f6a431694c9e573
pavithrachamala <56586609+pavithrachamala@users.noreply.github.com>
2020-06-06 08:22:33 +0530

    Add files via upload

---
+++ copyofbookshopmaintenceapplicationprovider/mysql-pact/sql-scripts/InsertBooks.sql
@@ -0,0 +1,2 @@
+INSERT INTO bookdetails (book_name, book_price , publisher_name, publishing_year) VALUES ('Spring', '333','raj','2008');
+INSERT INTO bookdetails (book_name, book_price ,  publisher_name,publishing_year) VALUES ('Head First Java', '498','mehtha','2005');
